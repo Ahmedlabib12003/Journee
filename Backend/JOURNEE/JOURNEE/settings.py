398fa3b0c648d3559366b372c74a91d9df8d75a8
Ahmed Labib <138168103+Ahmedlabib12003@users.noreply.github.com>
2024-05-23 21:34:58 +0300

    changing the db from sqlite to mysql

--- Backend/JOURNEE/JOURNEE/settings.py
+++ Backend/JOURNEE/JOURNEE/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'JOURNEE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'journee',
+        'USER': 'root',
+        'PASSWORD': 'Medo_mysql_2474',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
